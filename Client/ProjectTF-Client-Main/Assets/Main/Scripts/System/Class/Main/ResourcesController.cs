2605b43679c4094273e86893d9e6c93d550f7c89
B0ttle-Cat <kolazkr.lgb@gmail.com>
2024-11-23 02:35:51 +0900

    비동기 메서드 추가 및 로직 간소화
    # ResourcesController.cs
    > `Load`, `GetAsset`, `Instantiate` 메서드가 비동기 방식으로 변경되었습니다.
    > `Load` 메서드는 이제 `Awaitable<bool>`을 반환하며, 자산을 비동기적으로 로드합니다.
    > `GetAsset` 메서드는 이제 `Awaitable<T>`를 반환하며, 자산을 비동기적으로 가져옵니다.
    > `Instantiate` 메서드는 이제 `Awaitable<GameObject>`를 반환하며, 게임 오브젝트를 비동기적으로 인스턴스화합니다.
    > `Load` 메서드 내부에서 자산이 이미 로드되었는지 확인하는 로직이 간소화되었습니다.
    > `Complete` 메서드는 이제 로드된 자산을 확인하고, 로드가 성공했는지 여부를 반환합니다.
    > `WaitInstantiate` 메서드는 게임 오브젝트를 비동기적으로 인스턴스화하고, 결과를 반환합니다.
    > `Complete` 메서드는 이제 게임 오브젝트를 확인하고, 인스턴스화가 성공했는지 여부를 반환합니다.
    
    # IResourcesController.cs
    > `IResourcesController` 인터페이스에 비동기 메서드가 추가되었습니다.
    
    ---

--- Client/ProjectTF-Client-Main/Assets/Main/Scripts/System/Class/Main/ResourcesController.cs
+++ Client/ProjectTF-Client-Main/Assets/Main/Scripts/System/Class/Main/ResourcesController.cs
@@ -65,112 +65,125 @@ namespace TF.System
 			LoadAsync();
 			async void LoadAsync()
 			{
-				int already = loadStructList.FindIndex(x => x.path == path);
-				if(already >= 0)
-				{
-					if(loadStructList[already].loadAsset != null)
-					{
-						Debug.LogWarning($"Already Asset: {path}");
-						onComplete?.Invoke(true);
-					}
-					else
-					{
-						do
-						{
-							Debug.LogWarning($"Loading Asset: {path}");
-							await Awaitable.NextFrameAsync();
-						}
-						while(loadStructList[already].loadAsset == null);
-
-						onComplete?.Invoke(true);
-					}
-					return;
-				}
-
-				if(loadAPI == IResourcesController.AssetLoadAPI.AddressableAPI)
-				{
-					Addressables.LoadAssetAsync<T>(path).Completed += (AsyncOperationHandle<T> handle) => {
-						if(handle.Status == AsyncOperationStatus.Succeeded && handle.Result != null)
-						{
-							Complete(path, loadAPI, handle.Result);
-							onComplete?.Invoke(true);
-						}
-						else
-						{
-							Debug.LogError($"Failed Load Asset: {path}");
-							onComplete?.Invoke(false);
-						}
-					};
-				}
-				else if(loadAPI == IResourcesController.AssetLoadAPI.ResourcesAPI)
-				{
-					var wait = Resources.LoadAsync(path);
-					wait.completed += (AsyncOperation obj) => {
-						if(wait.asset != null)
-						{
-							Complete(path, loadAPI, wait.asset);
-						}
-						else
-						{
-							Debug.LogError($"Failed Load Asset: {path}");
-							onComplete?.Invoke(false);
-						}
-					};
-				}
-
-			}
-			void Complete(string path, IResourcesController.AssetLoadAPI loadAPI, Object loadAsset)
-			{
-				loadStructList.Add(new LoadStruct(path, loadAsset));
+				bool load = await Load<T>(path, loadAPI);
+				onComplete?.Invoke(false);
 			}
 		}
 		public void GetAsset<T>(string path, IResourcesController.AssetLoadAPI loadAPI, Action<T> onComplete) where T : Object
 		{
-			Load<T>(path, loadAPI, (result) => {
-				if(result)
-				{
-					var find = loadStructList.Find(find=>find.path == path);
-					if(find is not null and T tFind)
-					{
-						onComplete?.Invoke(tFind);
-						return;
-					}
-				}
-				onComplete?.Invoke(null);
-			});
+			GetAssetAsync();
+			async void GetAssetAsync()
+			{
+				T load = await GetAsset<T>(path, loadAPI);
+				onComplete?.Invoke(load);
+			}
 		}
 		public void Instantiate(string path, IResourcesController.AssetLoadAPI loadAPI, Vector3 position, Quaternion rotation, Transform parent, Action<GameObject> onComplete)
 		{
-			GetAsset<GameObject>(path, loadAPI, (loadObject) => {
-				InstantiateAsync(loadObject);
-			});
-			async void InstantiateAsync(GameObject loadObject)
+			InstantiateAsync();
+			async void InstantiateAsync()
+			{
+				var instantiate = await Instantiate(path, loadAPI, position, rotation, parent);
+				onComplete?.Invoke(instantiate);
+			}
+		}
+
+
+		public async Awaitable<bool> Load<T>(string path, IResourcesController.AssetLoadAPI loadAPI) where T : Object
+		{
+			if(string.IsNullOrWhiteSpace(path)) return false;
+
+			int already = loadStructList.FindIndex(x => x.path == path);
+			if(already >= 0)
+			{
+				if(loadStructList[already].loadAsset != null)
+				{
+					Debug.LogWarning($"Already Asset: {path}");
+				}
+				else
+				{
+					do
+					{
+						Debug.LogWarning($"Loading Asset: {path}");
+						await Awaitable.NextFrameAsync();
+					}
+					while(loadStructList[already].loadAsset == null);
+				}
+				return false;
+			}
+
+			if(loadAPI == IResourcesController.AssetLoadAPI.AddressableAPI)
+			{
+				var handle = Addressables.LoadAssetAsync<T>(path);
+				await handle.Task;
+				return Complete(path, loadAPI, handle.Status == AsyncOperationStatus.Succeeded ? handle.Result : null);
+			}
+			else if(loadAPI == IResourcesController.AssetLoadAPI.ResourcesAPI)
+			{
+				var handle = Resources.LoadAsync(path);
+				await handle;
+				return Complete(path, loadAPI, handle.asset);
+			}
+
+			return false;
+
+			bool Complete(string path, IResourcesController.AssetLoadAPI loadAPI, Object loadAsset)
+			{
+				if(loadAsset == null)
+				{
+					Debug.LogError($"Failed Load Asset: {path}");
+					return false;
+				}
+				loadStructList.Add(new LoadStruct(path, loadAsset));
+				return true;
+			}
+		}
+		public async Awaitable<T> GetAsset<T>(string path, IResourcesController.AssetLoadAPI loadAPI) where T : Object
+		{
+			bool load = await Load<T>(path, loadAPI);
+			if(load)
+			{
+				var find = loadStructList.Find(find=>find.path == path);
+				if(find is not null and T tFind)
+				{
+					return tFind;
+				}
+			}
+			return null;
+		}
+		public async Awaitable<GameObject> Instantiate(string path, IResourcesController.AssetLoadAPI loadAPI, Vector3 position, Quaternion rotation, Transform parent)
+		{
+			GameObject loadObject = await GetAsset<GameObject>(path, loadAPI);
+			GameObject instantiateObject = await WaitInstantiate(loadObject);
+
+			return instantiateObject;
+
+			async Awaitable<GameObject> WaitInstantiate(GameObject loadObject)
 			{
 				if(loadObject == null)
 				{
 					Debug.LogError($"Failed Is Not : GameObject");
-					onComplete?.Invoke(null);
-					return;
+					return null;
 				}
 
 				if(loadAPI == IResourcesController.AssetLoadAPI.AddressableAPI)
 				{
 					GameObject obj = await Addressables.InstantiateAsync(path, position, rotation, parent).Task;
-					Complete(path, obj);
+					return Complete(path, obj);
 				}
 				else if(loadAPI == IResourcesController.AssetLoadAPI.ResourcesAPI)
 				{
 					var wait = await Object.InstantiateAsync(loadObject, 0, parent, position, rotation);
-					Complete(path, wait[0]);
+					return Complete(path, wait[0]);
 				}
+				return null;
 			}
 
-			void Complete(string path, GameObject newGameObject)
+			GameObject Complete(string path, GameObject newGameObject)
 			{
 				if(newGameObject == null)
 				{
-					onComplete?.Invoke(null);
-					return;
+					return null;
 				}
 
 				int index = instantiateStructList.FindIndex(find => find.path == path);
@@ -185,9 +198,10 @@ namespace TF.System
 				resourcesState.Init(this, path);
 				findStruct.instantiateList.Add(newGameObject);
 
-				onComplete?.Invoke(newGameObject);
+				return newGameObject;
 			}
 		}
+
 		public void DestroyObject(string path, GameObject asset)
 		{
 			int index = instantiateStructList.FindIndex(find => find.path == path);
--- Client/ProjectTF-Client-Main/Assets/Main/Scripts/System/Interface/Main/IResourcesController.cs
+++ Client/ProjectTF-Client-Main/Assets/Main/Scripts/System/Interface/Main/IResourcesController.cs
@@ -19,6 +19,9 @@ namespace TF.System
 		public void Load<T>(string path, AssetLoadAPI loadAPI, Action<bool> onComplete) where T : Object;
 		public void GetAsset<T>(string path, AssetLoadAPI loadAPI, Action<T> onComplete) where T : Object;
 		public void Instantiate(string path, AssetLoadAPI loadAPI, Vector3 position, Quaternion rotation, Transform parent, Action<GameObject> onComplete);
+		public Awaitable<bool> Load<T>(string path, AssetLoadAPI loadAPI) where T : Object;
+		public Awaitable<T> GetAsset<T>(string path, AssetLoadAPI loadAPI) where T : Object;
+		public Awaitable<GameObject> Instantiate(string path, AssetLoadAPI loadAPI, Vector3 position, Quaternion rotation, Transform parent);
 		void DestroyObject(string path, GameObject asset);
 	}
 }
