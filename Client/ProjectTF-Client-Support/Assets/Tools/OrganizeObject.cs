89889c7a340814d36b32a514a4dfada4b1d2e2a5
B0ttle-Cat <kolazkr.lgb@gmail.com>
2024-12-28 01:21:54 +0900

    프리팹 및 스크립트 수정 사항 반영
    # .cs 파일
    > OrganizeObject.cs: 자식 오브젝트 정렬 및 이름 변경 기능 추가. X축 기준으로 정렬하고 "Room_Prop" 형식으로 이름 변경. 경계(Bounds) 계산 로직 포함.
    
    # .unity 파일
    > PropTestScene.unity: 실질적인 변경 없음.
    
    ---
    
    # 프리팹 수정 사항
    > 여러 프리팹에서 `m_ObjectHideFlags`가 1에서 0으로 변경되어 객체가 숨겨지지 않도록 설정됨.
    > `m_CorrespondingSourceObject`, `m_PrefabInstance`, `m_PrefabAsset`와 같은 새로운 속성이 추가됨.
    > `serializedVersion`이 증가하여 프리팹의 버전이 업데이트됨.
    > `m_LocalRotation`의 값이 변경되어 객체의 회전 상태가 초기화됨.
    > `MeshFilter`, `MeshRenderer`, `MeshCollider`와 같은 컴포넌트에서 여러 속성이 추가되거나 수정됨.
    > 여러 프리팹에서 `m_DynamicOccludee`, `m_StaticShadowCaster`, `m_RayTracingMode`와 같은 새로운 렌더링 관련 속성이 추가됨.
    > `m_Mesh` 속성이 업데이트되어 새로운 메쉬 데이터가 적용됨.

---
+++ Client/ProjectTF-Client-Support/Assets/Tools/OrganizeObject.cs
@@ -0,0 +1,123 @@
+﻿using System.Linq;
+
+using Sirenix.OdinInspector;
+
+using UnityEngine;
+
+public class OrganizeObject : MonoBehaviour
+{
+	[Button("OrganizeChildrenObjects")]
+	public void OrganizeChildrenObjects()
+	{
+		int childCount = transform.childCount;
+		Bounds[] childBounds = new Bounds[childCount];
+
+		// 1. 직속 자식들의 Bounds를 계산하여 배열에 저장
+		for(int i = 0 ; i < childCount ; i++)
+		{
+			var childTr = transform.GetChild(i);
+			var childObj = childTr.gameObject;
+
+			// 각 자식의 Bounds를 계산
+			Bounds? bounds = null;
+			Bounds[] tempBounds = childObj.GetComponentsInChildren<MeshRenderer>(true)
+				.Select(r => r.bounds)
+				.Concat(childObj.GetComponentsInChildren<SkinnedMeshRenderer>(true)
+				.Select(r => r.bounds))
+				//.Concat(childObj.GetComponentsInChildren<Collider>(true)
+				//.Select(c => c.bounds))
+				.ToArray();
+
+			// 모든 bounds를 결합하여 하나의 bounds로 만듦
+			foreach(var bound in tempBounds)
+			{
+				if(bounds.HasValue)
+				{
+					Bounds value = bounds.Value;
+					value.Encapsulate(bound);
+					bounds = value;
+				}
+				else
+				{
+					bounds = bound;
+				}
+			}
+
+			// bounds 배열에 저장
+			if(bounds.HasValue)
+			{
+				childBounds[i] = bounds.Value;
+			}
+			else
+			{
+				Debug.LogError($"No Bounds: {childObj.name}");
+			}
+		}
+
+		// 2. 직속 자식들을 X축을 기준으로 일렬로 정렬
+		float totalWidth = 0f;
+		for(int i = 0 ; i < childCount ; i++)
+		{
+			totalWidth += childBounds[i].size.x; // 각 자식의 너비를 합산
+		}
+
+		float currentX = -totalWidth / 2f; // 정렬 시작 위치 (왼쪽 끝에서 시작)
+
+		// 3. 자식들의 위치를 일렬로 정렬
+		for(int i = 0 ; i < childCount ; i++)
+		{
+			var childTr = transform.GetChild(i);
+			Bounds bound = childBounds[i];
+
+			// 현재 자식의 위치를 조정 (X축으로 이동)
+			childTr.position = new Vector3(currentX + bound.extents.x, childTr.position.y, childTr.position.z);
+
+			// X 위치를 다음 자식의 시작 위치로 업데이트
+			currentX += bound.size.x;
+		}
+	}
+
+
+	[Button("SortChildrenByName")]
+	public void SortChildrenByName()
+	{
+		var children = Enumerable.Range(0, transform.childCount)
+			.Select(i => transform.GetChild(i))
+			.OrderBy(child => child.name)
+			.ToList();
+
+		for(int i = 0 ; i < children.Count ; i++)
+		{
+			children[i].SetSiblingIndex(i);
+		}
+	}
+
+
+
+	[Button("ReplaceChildName")]
+	public void ReplaceChildName()
+	{
+		string Room_Prop_Index = "Room_Prop_{0:0000}";
+
+		int childCount = transform.childCount;
+		for(int i = 0 ; i < transform.childCount ; i++)
+		{
+			var childTr = transform.GetChild(i);
+			var childObj = childTr.gameObject;
+
+			if(!childObj.name.StartsWith("Room_Prop"))
+			{
+				GameObject propPivot = new GameObject(string.Format(Room_Prop_Index, i));
+				propPivot.transform.SetParent(transform);
+				propPivot.transform.SetSiblingIndex(i);
+				propPivot.transform.position = childTr.position;
+
+				childTr.SetParent(propPivot.transform);
+			}
+			else
+			{
+				childObj.name = string.Format(Room_Prop_Index, i);
+			}
+		}
+	}
+}
